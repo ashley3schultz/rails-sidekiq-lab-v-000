6eca182a2601974d0a4de613ee7dd93e098ab6a9
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-06 18:54:12 +0000

    automatically backed up by learn

---
+++ app/workers/Songs_worker.rb
@@ -0,0 +1,11 @@
+class SongWorker
+  require 'csv'
+  include Sideiq::Worker
+
+  def perform(file)
+    CSV.foreach(file, headers: true) do |song|
+      artist = Artist.find_or_create_by(name: song[1])
+      Song.create(title: song[0], artist_id: artist.id)
+    end
+  end 
+end 
